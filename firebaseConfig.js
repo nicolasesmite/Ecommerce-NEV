8cc70f07c019624bc8d07f77ee349ab89f784c22
Nicolas Esmite <nicolasemanuelesmitevaldez@gmail.com>
2023-09-17 21:37:14 -0300

    trabajando con el login

---
+++ firebaseConfig.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "firebase/app";
+import {signInWithEmailAndPassword, getAuth, signOut, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword} from "firebase/auth"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJu-hmno4smLO26TLwYX3TQFFcdzdyAfA",
+  authDomain: "jakemate-6e892.firebaseapp.com",
+  projectId: "jakemate-6e892",
+  storageBucket: "jakemate-6e892.appspot.com",
+  messagingSenderId: "117673521408",
+  appId: "1:117673521408:web:9afb6a329bfacb87d743e5"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+//Using firebase services
+
+export const onSignIn = async ({email, password}) => {
+    try {
+        let res = await signInWithEmailAndPassword(auth, email, password)
+        return res 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+let googleProvider = new GoogleAuthProvider()
+
+export const loginGoogle = async () =>{
+
+    const res = await signInWithPopup(auth, googleProvider);
+}
+
+export const logOut = ()=>{
+
+    signOut(auth);
+
+}
+
+export const signUp = async ({email, password}) =>{
+
+    try {
+        let res =  await createUserWithEmailAndPassword(auth, email, password)
+        return res
+    } catch (error) {
+        console.log(res)
+    }
+
+   
+}
