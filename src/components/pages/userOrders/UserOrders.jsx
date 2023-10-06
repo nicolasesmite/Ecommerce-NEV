e0533e87c009c2a790dfcb739c7882176b84d512
Nicolas Esmite <nicolasemanuelesmitevaldez@gmail.com>
2023-10-06 00:59:32 -0300

    trabajando en mis ordenes

--- src/components/pages/userOrders/UserOrders.jsx
+++ src/components/pages/userOrders/UserOrders.jsx
@@ -1,13 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { db } from "../../../../firebaseConfig";
 import { getDocs, collection, query, where } from "firebase/firestore";
+import { AuthContext } from "../../../context/AuthContext";
+import UserOrdersContainer from "./UserOrdersContainer";
 
 const UserOrders = () => {
   const [myOrders, setMyOrders] = useState([]);
+  const { user } = useContext(AuthContext);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const orderCollectios = collection(db, "orders");
+    let ordersFiltered = query(
+      orderCollectios,
+      where("buyer.email", "==", user.email)
+    );
+    getDocs(ordersFiltered)
+      .then((res) => {
+        const newArray = res.docs.map((order) => {
+          return { ...order.data(), id: order.id };
+        });
 
-  return <div>UserOrders</div>;
+        setMyOrders(newArray);
+      })
+      .catch((error) => console.log(error));
+  }, [user.email]);
+
+  return <UserOrdersContainer myOrder={myOrders} />;
 };
 
 export default UserOrders;
---
+++ src/components/pages/userOrders/UserOrdersContainer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const UserOrdersContainer = ({ myOrder }) => {
+  return (
+    <div>
+      <h2>Estoy en mis ordenes</h2>
+
+      {myOrder.map((order) => {
+        console.log(order.date);
+        return (
+          <div key={order.id}>
+            <h2>${order.total}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default UserOrdersContainer;
